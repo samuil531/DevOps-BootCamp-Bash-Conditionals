a3f93baddc59f66c62372e6cbfa03eb71b024b25
samuil531 <108720085+samuil531@users.noreply.github.com>
2022-07-10 13:45:22 +0300

    Update cond1.sh

--- cond1.sh
+++ cond1.sh
@@ -2,4 +2,14 @@
 
 IFS=',' read -r -a array <<< "$1"
 
-#Write your code here
+str=$@
+sum=
+IFS=,
+for i in $str
+do
+        if [ $(($i%2)) -eq 0 ]
+        then
+                sum=$[$sum+$i]
+        fi
+done
+echo $sum
