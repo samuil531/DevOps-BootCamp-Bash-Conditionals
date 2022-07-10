cc425ce586980ee9204f0f36b0635de9dde35c92
samuil531 <108720085+samuil531@users.noreply.github.com>
2022-07-10 13:57:34 +0300

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-# Place your code here
+input=$1
+type=${input: -1}
+temp=${input::-1}
+
+if [[ $type == "K" ]]
+then
+    echo "Kelvin to Celsius"
+    result=$(($temp-273))
+    echo -n "$result"
+    echo "C"
+elif [[ $type == "C" ]]
+then
+    echo "Celsius to Kelvin"
+    result=$(($temp+273))
+    echo -n "$result"
+    echo "K"
+fi
